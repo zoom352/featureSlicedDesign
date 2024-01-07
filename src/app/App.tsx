edefe1db8d2200c37f564212aa758f45e06024ec
Дмитрий <d.kulinich@nlabteam.com>
2024-01-07 16:57:13 +0300

    add ts for routes

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+import { Link, Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { Suspense } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
+
+const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,14 +1,10 @@
 import { FC, useMemo, useState } from "react"
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext"
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "../lib/ThemeContext"
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
 const ThemeProvider: FC = ({children}) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
-
-//   const toggleTheme = () => {
-//     setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-//   }
 
   const defaultProps = useMemo(() => ({
     theme: theme,
